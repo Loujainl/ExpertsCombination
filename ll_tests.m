0b9fd12631cc9934070b9f14fd07001b533bc74d
Nouveau <Nouveau@wifiroam044067.univ-st-etienne.fr>
2018-05-29 11:44:40 +0200

    included bestfield in main run for easier comparision, enhancements

--- ll_tests.m
+++ ll_tests.m
@@ -6,6 +6,9 @@ rng(1);
 n = 20;      %number of training data
 p = 1000;     %number of coefficients or features
 p_star  = 150;
+disp(['Number of training data:', num2str(n)]);
+disp(['Number of features:', num2str(p)]);
+
 disp(['Number of Relevant Features:',num2str(p_star)]);
 
 %generate unknown, true coefficient values (only first p_star are non-zero)
@@ -16,22 +19,33 @@ disp(['Number of Experts:', num2str(experts_nu)]);
 
 % number of questions to ask equal number of relevant features
 budget = p_star;
-run_times = 100;
+run_times = 10;
 disp(['number of runtimes: ',num2str(run_times)]);
 
 %start big loop
 MSE = zeros(run_times, experts_nu);
 MSE_maj = zeros(run_times, 1);
+MSE_Best = zeros(run_times,1);
+MSE_no_fb = zeros(run_times,1);
+MSE_ridge_nofb =zeros(run_times,1) ;
 
 for iter= 1:run_times
     
         %create n, p dimensional training data
         x = randn(n,p);
         y = normrnd(x*w_star, 1);
+        
+        % Normalize  training data
+       % x = (x-mean(x))/std(x);
+       % y = (y-mean(y))/std(y);
 
         %create some test data
         x_test = randn(1000,p);
         y_test = normrnd(x_test*w_star, 1);
+        
+        % Normalise test data
+      %  x_test = (x_test-mean(x_test))/std(x_test);
+      %  y_test = (y_test-mean(y_test))/std(y_test);
 
         %initialize the inputs
         pr  = struct('tau2', 1^2 , 'eta2',0.1^2,'p_u', 0.95, 'rho', p_star/p, ...
@@ -88,14 +102,55 @@ for iter= 1:run_times
         [fa_fb, si, converged, subfunctions] = linreg_sns_ep(y, x, pr, op, [], majority_feedback, []);
         MSE_with_majority_fb = mean((x_test*fa_fb.w.Mean- y_test).^2); 
     
+        % Clustering features 
+        field_size = round(budget/experts_nu); 
+        % numbre of fields = number of experts
+        field_acc = zeros(experts_nu);
+        
+        for ex=1:experts_nu
+            tempo = all_feedbacks(:,ex)';
+        %calculating accuracy per field for each expert
+            field_acc(:,ex) = mean(reshape(tempo(1:field_size * floor(numel(tempo) / field_size)), [], field_size), 2);
+        end
+        
+        [maxx, m_indx] = max(field_acc,[],2);
+        % retrieving the best feedback from max field_accuracy
+        best_feed = zeros(budget,1);
+        
+        for count = 1: experts_nu
+            best_feed(((count-1)*field_size)+1: count*field_size,:) = all_feedbacks(((count-1)*field_size)+1: count*field_size , m_indx(count));
+            % best_feed(x-1)*field_size)+1: x*field_size) = [best_feed; temp];
+        end
+
+    best_feed = [[best_feed(1:budget,:); zeros(p-budget,1)], [1:p]' ];
+    best_feed_level =mean(best_feed(1:budget,1),1);
+    [fa_fb, si, converged, subfunctions] = linreg_sns_ep(y, x, pr, op, [], best_feed, []);
+    MSE_with_best_field_fb = mean((x_test*fa_fb.w.Mean- y_test).^2); 
+   % disp(['Spike-and-slab field feedback feedback:',num2str(MSE_with_best_field_fb)]);    
+   
+    %results without feedback (only spike and slab model)
+    [fa, si, converged, subfunctions] = linreg_sns_ep(y, x, pr, op, [], [], []);
+    MSE_without_fb = mean((x_test*fa.w.Mean- y_test).^2);
+    %ridge regression solution
+    w_ridge = inv(eye(p) + (x'*x)) * (x'*y);
+    MSE_ridge = mean((x_test*w_ridge- y_test).^2);
+        
         MSE_maj(iter) = MSE_with_majority_fb ;
         MSE(iter,:) = MSE_with_multi_fb;
+        MSE_Best(iter) = MSE_with_best_field_fb; 
+        MSE_no_fb(iter) = MSE_without_fb;
+        MSE_ridge_nofb(iter) = MSE_ridge; 
+        
 end
 
     MSE_avg = mean(MSE,1);
     disp(['Spike-and-slab with user feedback  = ',num2str(MSE_avg)]);
     MSE_maj_avg = mean(MSE_maj);
     disp(['Spike-and-slab with Majority Vote feedback  = ',num2str(MSE_maj_avg)]);
+    MSE_Best_avg = mean(MSE_Best);
+    disp(['Spike-and-slab with Best Field feedback  = ',num2str(MSE_Best_avg)]);
+    
+    
     plot(experts_level,MSE_avg);
     xlabel('Expert Confidality');
     ylabel('Error');
@@ -104,32 +159,32 @@ end
     majority_confidality =mean(majority_feedback(1:budget,1),1) ;
     plot(majority_confidality,MSE_maj_avg,'r*','MarkerSize',10);
     
-    
-    %results without feedback (only spike and slab model)
-    [fa, si, converged, subfunctions] = linreg_sns_ep(y, x, pr, op, [], [], []);
-    MSE_without_fb = mean((x_test*fa.w.Mean- y_test).^2);
-    %ridge regression solution
-    w_ridge = inv(eye(p) + (x'*x)) * (x'*y);
-    MSE_ridge = mean((x_test*w_ridge- y_test).^2);
+    plot(best_feed_level,MSE_Best_avg,'gx','MarkerSize',12);
+
 
     disp('Mean Squared Error on test data:')
-    disp(['Spike-and-slab without user feedback:',num2str(MSE_without_fb)])
-    plot(MSE_without_fb,'ks','MarkerSize',11);
-    disp(['Ridge regression:',num2str(MSE_ridge)])
-    plot(MSE_ridge,'bx','MarkerSize',12);
+    MSE_no_fb_avg = mean(MSE_no_fb);
+    disp(['Spike-and-slab without user feedback:',num2str(MSE_no_fb_avg)])
+    plot(0,MSE_no_fb_avg,'ks','MarkerSize',16);
+    
+    MSE_ridge_nofb_avg = mean(MSE_ridge_nofb);
+    disp(['Ridge regression:',num2str(MSE_ridge_nofb_avg)])
+    plot(0,MSE_ridge_nofb_avg,'rs','MarkerSize',16);
 
 
 
 
 % % Results: 
-% N= 20; p=1000;
-% Number of Relevant Features:150
+%Number of training data:20
+%Number of features:1000
+%Number of Relevant Features:150
 % Number of Experts:5
 % experts confidality : [0.77 0.7 0.65 0.5 0.4]
-% number of runtimes:100
-% Spike-and-slab with user feedback  = 135.035      136.0079       137.785      140.2984      142.1207
-% Spike-and-slab majority feedback:135.0031
+% number of runtimes:10
+% Spike-and-slab with user feedback  = 130.5198      132.3882      134.7141      136.6413      138.5169
+% Spike-and-slab with Majority Vote feedback  = 132.2505
+%Spike-and-slab with Best Field feedback  = 132.5059
 % Mean Squared Error on test data:
-% Spike-and-slab without user feedback:145.6689
-% Ridge regression:145.4891
+% Spike-and-slab without user feedback:143.233
+% Ridge regression:143.2995
 
